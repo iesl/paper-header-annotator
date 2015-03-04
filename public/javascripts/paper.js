bbaa514d88374a9460c361de87ef19d8a72a5ee2
k8si <ksilvers@cs.umass.edu>
2015-03-04 02:11:36 -0500

    messing around with css

--- public/javascripts/paper.js
+++ public/javascripts/paper.js
@@ -1,3 +1,22 @@
+/*
+TODO change annot color based on label selection
+TODO only display first-ish page
+TODO cycle thru labels using tab key?
+TODO display annotations somewhere for review
+TODO "save doc" --> write annotations and etc out somewhere
+ */
+
+var labelColors = {
+    "author": "red",
+    "title": "blue",
+    "email": "green",
+    "abstract": "orange",
+    "institution": "cyan",
+    "address": "magenta"
+};
+
+var actions = [];
+
 $(document).ready(function() {
     function printMe(selection) {
         console.log(selection[0].length);
@@ -14,12 +33,10 @@ $(document).ready(function() {
         }
     }
 
-    /* toggle the color on mouseover
-     * TODO change color based on label selection
-     * */
-    var currLabel = "author";
+    /* toggle the color on mouseover */
+    var currLabel;
     var stageStyle = "fill:gray;text-decoration:underline;";
-    var commitStyle = "fill:red;text-decoration:none";
+
     d3.selectAll('svg tspan')
         .on('mouseover', function() {
             var sel = d3.select(this);
@@ -36,15 +53,21 @@ $(document).ready(function() {
             }
         });
 
-    /* Hit the keyboard to double click to commit annotation */
-    $(this).keypress(function() {
-        d3.selectAll('.staged').attr('style', commitStyle).attr('class', 'selected').attr('id', currLabel);
-        //var label = sel.attr('id');
-        //var jq = '#'+label;
-        //$(jq).append('<p>'+sel.text()+'</p>');
-    });
+    ///* Hit the keyboard or double click to commit annotation */
+    //$(this).keypress(function() {
+    //    var commitStyle = "fill:" + labelColors[currLabel] + ";text-decoration:none";
+    //    d3.selectAll('.staged').attr('style', commitStyle).attr('class', 'selected').attr('id', currLabel);
+    //    //var label = sel.attr('id');
+    //    //var jq = '#'+label;
+    //    //$(jq).append('<p>'+sel.text()+'</p>');
+    //});
     $(this).dblclick(function() {
-        d3.selectAll('.staged').attr('style', commitStyle).attr('class', 'selected').attr('id', currLabel);
+        if (currLabel == undefined) alert("select a label to annotate first");
+        var commitStyle = "fill:" + labelColors[currLabel] + ";text-decoration:none";
+        actions.push(d3.selectAll('.staged').attr('style', commitStyle).attr('class', 'selected').attr('id', currLabel));
+        //actions.push(d3.selectAll('.selected'));
+        //d3.selectAll('.staged').attr('class', 'selected').attr('id', currLabel);
+
     });
 
     /* buttons */
@@ -57,6 +80,23 @@ $(document).ready(function() {
     $('#refresh-button').click(function() {
         d3.selectAll('.staged').attr('style', null).attr('class', '');
         d3.selectAll('.selected').attr('style', null).attr('class', '');
+    });
+    $('#undo-button').click(function() {
+       var sel = actions.pop();
+        console.log(sel[0][0]);
+        d3.select(sel[0][0]).attr('style', null).attr('class', '');
+    });
+    $('form').submit(function() {
+        var checked = $('input:radio:checked').val();
+        currLabel = checked;
+        var newTxt = '<div id=curr-label>annotating: <span class="inner" id="' + checked + '">' + checked + '</span></div>';
+        $('#curr-label').replaceWith(newTxt);
+        //console.log(newTxt);
+        ////$('#curr-label').text("currently annotating: ");
+        //console.log($('#curr-label.inner'));
+        //$('#curr-label.inner').replaceWith(newTxt);
+        $('#clear-button').click();
+        return false;
     });
 
 });
